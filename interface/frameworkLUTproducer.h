e4278bdb4d857d4b966889ffeb164c787f3fc011
Neil <neil_schroeder@ymail.com>
2019-03-28 10:29:32 -0500

    missed a file from most recent changes

---
+++ interface/frameworkLUTproducer.h
@@ -0,0 +1,11 @@
+#include "TH1.h"
+#include "TH2.h"
+#include <vector>
+
+
+class myLookUpTableProducer{
+
+    public:
+        void produce_LookUpTables(std::string, bool);
+        void plot_LookUpTable(TH2F*, std::string, double, double, double, double);
+};
